82357c67c3408c0c7e922962b9d7122690078aec
Ivan Mahadika <ivanmahadika05@gmail.com>
2024-12-21 22:43:04 +0700

    tugas praktikum 1

---
+++ tp1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nama, nim;
+    cout << "Siapa nama anda?";
+    cin >> nama;
+    cout << "Berapa NIM anda?";
+    cin >> nim;
+    cout << "Nama saya: " << endl;
+    cout << "Nim saya: " << endl;
+    return 0;
+}
